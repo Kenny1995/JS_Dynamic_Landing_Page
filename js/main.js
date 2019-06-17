23b364ea8ee93fb10f77556ba20b2961ef5674a5
Benny Katchy <kennethmichael11@gmail.com>
2019-06-17 11:05:47 +0100

    Working on a dynamic background that changes according to the time of the day

--- js/main.js
+++ js/main.js
@@ -1,39 +1,48 @@
 // DOM Elements Selection
-const time = document.getElementById('time'),
-    greeting = document.getElementById('greetings'),
-    name = document.getElementById('name'),
-    focus = document.getElementById('focus');
-
+const time = document.getElementById("time"),
+  greeting = document.getElementById("greetings"),
+  name = document.getElementById("name"),
+  focus = document.getElementById("focus");
 
 // Show Time
 const ShowTime = () => {
-    let today = new Date(),
-        hour = today.getHours(),
-        min = today.getMinutes(),
-        sec = today.getSeconds();
+  let today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds();
 
-    // Set AM or PM
-    const amPm = hour >= 12 ? 'PM' : 'AM';
+  // Set AM or PM
+  const amPm = hour >= 12 ? "PM" : "AM";
 
-    // 12hr Format
-    hour = hour % 2 || 12;
+  // 12hr Format
+  hour = hour % 2 || 12;
 
-    //ouput Time
-    time.innerHTML = `${AddZero(hour)}<span>:</span>${AddZero(min)}<span>:</span>${AddZero(sec)}`;
+  //ouput Time
+  time.innerHTML = `${AddZero(hour)}<span>:</span>${AddZero(
+    min
+  )}<span>:</span>${AddZero(sec)}`;
 
-    setTimeout(ShowTime, 1000);
-}
+  setTimeout(ShowTime, 1000);
+};
 
 // Add Zeros to the time base
-const AddZero = (n) => {
-    return (parseInt(n, 10) < 10 ? '0' : '') + n
-}
+const AddZero = n => {
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+};
 
 // set Background and Greeting
 const setBgGreet = () => {
-    let today = new Date(),
-        hour = today.getHours()
-}
+  let today = new Date(),
+    hour = today.getHours();
+  if (hour <= 12) {
+    document.body.style.backgroundImage = "url('/img/morning.jpg')";
+  } else if (hour <= 18) {
+    document.body.style.backgroundImage = "url('/img/afternoon.jpg')";
+  } else {
+    document.getElementsByTagName("body").style.backgroundImage =
+      "url('/img/night.jpg')";
+  }
+};
 
 // Run
 ShowTime();
