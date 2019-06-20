e91558807801359ea38b6d6df252973df83ebe40
Benny Katchy <kennethmichael11@gmail.com>
2019-06-20 05:28:09 +0100

    Retargeted the image url

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ const setBgGreet = () => {
   let today = new Date(),
     hour = today.getHours();
   if (hour < 12) {
-    document.body.style.backgroundImage = "url('../img/morning.jpg')";
+    document.body.style.backgroundImage = "url('/img/morning.jpg')";
     greeting.textContent = "Good Morning";
   } else if (hour < 18) {
     document.body.style.backgroundImage = "url('/img/afternoon.jpg')";
