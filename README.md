2a013a26e5aac8f97d0e557fc94888339a1cb905
Benny Katchy <kennethmichael11@gmail.com>
2019-08-13 14:01:49 +0100

    Added features to change the name and also made it a chrome Extention

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# JS_Dynamic_Landing_Page 
+# JS_Dynamic_Landing_Page
+
 In this project i built a homepage or landing page with the time that one can interact with and save any name/focus to local storage.
+
+This Project is also made a chrome extention such that when installed it will become the landing page of the installer.
--- js/main.js
+++ js/main.js
@@ -47,6 +47,55 @@ const setBgGreet = () => {
   }
 };
 
+// Get Name
+function getName() {
+  if (localStorage.getItem("name") === null) {
+    name.textContent = "[Enter Name]";
+  } else {
+    name.textContent = localStorage.getItem("name");
+  }
+}
+
+// Set Name
+function setName(e) {
+  if (e.type === "keypress") {
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("name", e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem("name", e.target.innerText);
+  }
+}
+
+//get focus
+function getFocus() {
+  if (localStorage.getItem("focus") === null) {
+    focus.textContent = "[Enter Focus]";
+  } else {
+    focus.textContent = localStorage.getItem("focus");
+  }
+}
+
+//Set focus
+function setFocus(e) {
+  if (e.type === "keypress") {
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("focus", e.target.innerText);
+      focus.blur();
+    }
+  } else {
+    localStorage.setItem("focus", e.target.innerText);
+  }
+}
+
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+focus.addEventListener("keypress", setFocus);
+focus.addEventListener("blur", setFocus);
+
 // Run
 ShowTime();
 setBgGreet();
+getName();
+getFocus();
